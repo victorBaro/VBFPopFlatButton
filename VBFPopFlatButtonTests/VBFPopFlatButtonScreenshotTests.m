90c61281f4bc7ed4d5628ab58bcd1c1113bc75b0
Raimon Lapuente <r.lapuente@gmail.com>
2015-04-18 16:32:56 +0100

    Adding tests for close button type

--- VBFPopFlatButtonTests/VBFPopFlatButtonScreenshotTests.m
+++ VBFPopFlatButtonTests/VBFPopFlatButtonScreenshotTests.m
@@ -98,4 +98,29 @@ describe(@"Minus Button Type", ^{
     });
 });
 
+describe(@"Close Button Type", ^{
+    
+    __block UIView *view;
+    
+    beforeEach(^{
+        view = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 480.0)];
+        view.backgroundColor = [UIColor blueColor];
+    });
+    
+    it(@"plain button matches plain button", ^{
+        VBFPopFlatButton *button = [[VBFPopFlatButton alloc] initWithFrame:CGRectMake(50.0, 50.0, 40.0f, 40.0f) buttonType:buttonCloseType buttonStyle:buttonPlainStyle animateToInitialState:NO];
+        [view addSubview:button];
+//                expect(view).to.recordSnapshotNamed(@"closeButtonPlain");
+        expect(view).to.haveValidSnapshotNamed(@"closeButtonPlain");
+    });
+    
+    it(@"round default button matches round default button", ^{
+        VBFPopFlatButton *button = [[VBFPopFlatButton alloc] initWithFrame:CGRectMake(50.0, 50.0, 40.0f, 40.0f) buttonType:buttonCloseType buttonStyle:buttonRoundedStyle animateToInitialState:NO];
+        button.roundBackgroundColor = [UIColor whiteColor];
+        button.tintColor = [UIColor blueColor];
+        [view addSubview:button];
+//                expect(view).to.recordSnapshotNamed(@"closeButtonRound");
+        expect(view).to.haveValidSnapshotNamed(@"closeButtonRound");
+    });
+});
 SpecEnd
