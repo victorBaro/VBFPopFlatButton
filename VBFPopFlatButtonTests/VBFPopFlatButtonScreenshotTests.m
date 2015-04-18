9cd97ccd29f6282cb8f9e22910d1cf85db8ba353
Raimon Lapuente <r.lapuente@gmail.com>
2015-04-18 16:44:04 +0100

    UpBasic button tests

--- VBFPopFlatButtonTests/VBFPopFlatButtonScreenshotTests.m
+++ VBFPopFlatButtonTests/VBFPopFlatButtonScreenshotTests.m
@@ -280,4 +280,30 @@ describe(@"DownBasic Button Type", ^{
     });
 });
 
+describe(@"UpBasic Button Type", ^{
+    
+    __block UIView *view;
+    
+    beforeEach(^{
+        view = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 480.0)];
+        view.backgroundColor = [UIColor blueColor];
+    });
+    
+    it(@"plain button matches plain button", ^{
+        VBFPopFlatButton *button = [[VBFPopFlatButton alloc] initWithFrame:CGRectMake(50.0, 50.0, 40.0f, 40.0f) buttonType:buttonUpBasicType buttonStyle:buttonPlainStyle animateToInitialState:NO];
+        [view addSubview:button];
+//        expect(view).to.recordSnapshotNamed(@"upBasicButtonPlain");
+        expect(view).to.haveValidSnapshotNamed(@"upBasicButtonPlain");
+    });
+    
+    it(@"round default button matches round default button", ^{
+        VBFPopFlatButton *button = [[VBFPopFlatButton alloc] initWithFrame:CGRectMake(50.0, 50.0, 40.0f, 40.0f) buttonType:buttonUpBasicType buttonStyle:buttonRoundedStyle animateToInitialState:NO];
+        button.roundBackgroundColor = [UIColor whiteColor];
+        button.tintColor = [UIColor blueColor];
+        [view addSubview:button];
+//        expect(view).to.recordSnapshotNamed(@"upBasicButtonRound");
+        expect(view).to.haveValidSnapshotNamed(@"upBasicButtonRound");
+    });
+});
+
 SpecEnd
