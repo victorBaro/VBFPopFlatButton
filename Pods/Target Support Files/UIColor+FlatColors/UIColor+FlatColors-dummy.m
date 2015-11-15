49c19f4bb2787348e63dd0951e793cb45033d4db
victorBaro <fs.baro@gmail.com>
2015-11-15 20:36:15 +0100

    Revert "Update pods & delete test file"
    This reverts commit 68203f99020ef97c93d0c1ae597f556dae194cfc.

--- Pods/Target Support Files/UIColor+FlatColors/UIColor+FlatColors-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UIColor_FlatColors : NSObject
-@end
-@implementation PodsDummy_UIColor_FlatColors
-@end
---
+++ VBFPopFlatButtonTests/VBFPopFlatButtonTests.m
@@ -0,0 +1,35 @@
+//
+//  VBFPopFlatButtonTests.m
+//  VBFPopFlatButtonTests
+//
+//  Created by Victor Baro on 16/08/2014.
+//  Copyright (c) 2014 Victor Baro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface VBFPopFlatButtonTests : XCTestCase
+
+@end
+
+@implementation VBFPopFlatButtonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+
+
+@end
