68203f99020ef97c93d0c1ae597f556dae194cfc
victorBaro <fs.baro@gmail.com>
2015-11-15 20:05:49 +0100

    Update pods & delete test file

--- Pods/Target Support Files/Pods-VBFPopFlatButtonTests-OCMock/Pods-VBFPopFlatButtonTests-OCMock-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VBFPopFlatButtonTests_OCMock : NSObject
-@end
-@implementation PodsDummy_Pods_VBFPopFlatButtonTests_OCMock
-@end
---
+++ Pods/Target Support Files/UIColor+FlatColors/UIColor+FlatColors-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIColor_FlatColors : NSObject
+@end
+@implementation PodsDummy_UIColor_FlatColors
+@end
